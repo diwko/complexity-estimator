4a1ecab14f2bee9cb8dd2668ca3cf64efd3dd8bb
Dawid Siwko <dawid.siwko@gmail.com>
2017-05-07 12:01:21 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ComplexityEstimator',
+      version='0.1',
+      description='Estimate function complexity',
+      url='https://github.com/AGHPythonCourse2017/zad2-lysy352',
+      author='Dawid Siwko',
+      author_email='dawid.siwko@gmail.com',
+      license='MIT',
+      packages=['complexityestimator'],
+      zip_safe=False)
